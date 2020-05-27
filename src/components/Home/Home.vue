f66eaf0610b8869210ad60ad9cf70dc4762b1bbf
dmhoang <dmhoang08t1@gmail.com>
2020-05-27 15:33:51 +0700

    push source code

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1 class="home-title">Home page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'home',
+  data(){
+    return{}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "./Home.scss";
+  /* .home {
+
+  }
+  .home-title {
+      color: red;
+    } */
+
+</style>
