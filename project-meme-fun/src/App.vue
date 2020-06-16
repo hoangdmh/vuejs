d3a6c0b4c0678f5e2d2ce7b0ca0e2ede46fd467a
dmhoang <dmhoang08t1@gmail.com>
2020-06-16 10:40:35 +0700

    init code

---
+++ project-meme-fun/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
