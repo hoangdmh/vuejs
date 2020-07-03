f0eb0fa379c834e2d5ede3a1932f42d84f69511d
dmhoang <dmhoang08t1@gmail.com>
2020-07-03 15:51:22 +0700

    vatidate() in nuxt

---
+++ Nuxt/nuxt-first-leson/pages/desks/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Desks by id {{$route.params.id}}</h1>
+</template>
+
+<script>
+export default {
+  validate({ params }) {
+    //console.log(context);
+    return /^[0-9]{9,12}$/.test(params.id);
+  }
+};
+</script>
+
+<style>
+</style>
