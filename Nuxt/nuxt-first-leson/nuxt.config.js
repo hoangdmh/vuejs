765ec9a872c5ef4a2ab603f191436fa2ba13235b
dmhoang <dmhoang08t1@gmail.com>
2020-07-06 15:24:25 +0700

    add global css and font

--- Nuxt/nuxt-first-leson/nuxt.config.js
+++ Nuxt/nuxt-first-leson/nuxt.config.js
@@ -22,13 +22,16 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap' }
     ]
   },
   /*
   ** Global CSS
   */
   css: [
+    '~/assets/css/bootstrap.min.css',
+    '~/assets/css/font.css',
   ],
   /*
   ** Plugins to load before mounting the App
